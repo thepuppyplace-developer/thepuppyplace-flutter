de5086a3574dc36199ca3b87189e1389fe7ad09c
hpodong <hjangwoo@gmail.com>
2022-07-28 15:53:11 +0900

    push

--- lib/pages/my_page/app_info_page.dart
+++ lib/pages/my_page/app_info_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:thepuppyplace_flutter/util/common.dart';
+import 'package:thepuppyplace_flutter/util/png_list.dart';
 
 import '../../controllers/version/version_controller.dart';
 import '../../widgets/buttons/custom_text_button.dart';
@@ -23,6 +24,16 @@ class AppInfoPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Container(
+                constraints: BoxConstraints.expand(height: mediaHeight(context, 0.1)),
+                margin: (baseHorizontalPadding(context) * 10).add(baseVerticalPadding(context) * 2),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(PngList.app_logo)
+                    )
+                  ),
+              ),
               Row(
                 children: [
                   const Expanded(child: CustomTextButton('현재버전', null, color: Colors.black, alignment: Alignment.centerLeft)),
@@ -37,14 +48,32 @@ class AppInfoPage extends StatelessWidget {
               ),
               Row(
                 children: [
-                  const Expanded(child: CustomTextButton('개발자', null, color: Colors.black, alignment: Alignment.centerLeft)),
-                  Text('hpodong', style: CustomTextStyle.w500(context, color: CustomColors.hint))
+                  const Expanded(child: CustomTextButton('개발', null, color: Colors.black, alignment: Alignment.centerLeft)),
+                  Text('황장우, 이지환', style: CustomTextStyle.w500(context, color: CustomColors.hint))
                 ],
               ),
               Row(
                 children: [
-                  const Expanded(child: CustomTextButton('기획자', null, color: Colors.black, alignment: Alignment.centerLeft)),
-                  Text('hpodong', style: CustomTextStyle.w500(context, color: CustomColors.hint))
+                  const Expanded(child: CustomTextButton('디자인', null, color: Colors.black, alignment: Alignment.centerLeft)),
+                  Text('황영서', style: CustomTextStyle.w500(context, color: CustomColors.hint))
+                ],
+              ),
+              Row(
+                children: [
+                  const Expanded(child: CustomTextButton('기획', null, color: Colors.black, alignment: Alignment.centerLeft)),
+                  Text('김성은, 이하진, 이서진', style: CustomTextStyle.w500(context, color: CustomColors.hint))
+                ],
+              ),
+              Row(
+                children: [
+                  const Expanded(child: CustomTextButton('홍보', null, color: Colors.black, alignment: Alignment.centerLeft)),
+                  Text('유혜경, 김우식', style: CustomTextStyle.w500(context, color: CustomColors.hint))
+                ],
+              ),
+              Row(
+                children: [
+                  const Expanded(child: CustomTextButton('총괄', null, color: Colors.black, alignment: Alignment.centerLeft)),
+                  Text('GlobalQ Korea', style: CustomTextStyle.w500(context, color: CustomColors.hint))
                 ],
               ),
             ],
